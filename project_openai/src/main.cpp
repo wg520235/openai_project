463a94d6d56b66e171bda23f87baaa40a215d950
wg520235 <80668445+wg520235@users.noreply.github.com>
2024-03-18 19:37:56 +0800

    Update main.cpp
    modify

--- project_openai/src/main.cpp
+++ project_openai/src/main.cpp
@@ -2,15 +2,17 @@
 #include<stdlib.h>
 #include<unistd.h>
 #include"mqttclient.h"
-
+#include"chat.h"
 
 int main(int argc, char *argv[])
 {
-	if(argc < 2){
+	if(argc < 3){
 		printf("please input your api key, exmple:./OpenAi_Server sk-bfLnibcPiR....Fw4ub2BFSJk8 \n");
 		return 0;
 	}
-	MqttClient_Init(argv[1]);
+	
+
+	MqttClient_Init(argv[1], argv[2], argv[2]);
 	while(1)
 	{
 		sleep(1);
@@ -19,5 +21,3 @@ int main(int argc, char *argv[])
 	MqttClient_UnInit();
 	return 0;
 }
-
-
