adeb417772055d2cada8d8e4d8916c74ee0d63ec
wg520235 <80668445+wg520235@users.noreply.github.com>
2024-03-18 23:09:14 +0800

    Update main.cpp
    add main

--- project_openai/src/main.cpp
+++ project_openai/src/main.cpp
@@ -6,12 +6,12 @@
 int main(int argc, char *argv[])
 {
 	if(argc < 3){
-		printf("please input your api key, exmple:./OpenAi_Server sk-bfLnibcPiR....Fw4ub2BFSJk8 \n");
+		printf("please input your api key, exmple:./OpenAi_Server sk-bfLnibcPiR....Fw4ub2BFSJk8 gpt_model dalle_model \n");
 		return 0;
 	}
 	
 
-	MqttClient_Init(argv[1], argv[2], argv[2]);
+	MqttClient_Init(argv[1], argv[2], argv[3]);
 	while(1)
 	{
 		sleep(1);
